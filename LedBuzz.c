ad362552c182b8205968c2cfdc75ea5cf8e3276e
Wilson Oliveira Lima <oliveiralima@protonmail.com>
2025-01-19 15:55:27 -0300

    Add GPIO initialization and command processing for LED and buzzer control

--- LedBuzz.c
+++ LedBuzz.c
@@ -2,39 +2,56 @@
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
+// Definições da UART
+// Por padrão, a UART stdout é `uart0`, então usaremos a segunda
 #define UART_ID uart1
 #define BAUD_RATE 115200
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+// Usar os pinos 4 e 5 para UART1
+// Os pinos podem ser alterados, veja a tabela de seleção de função GPIO no datasheet para informações sobre atribuições de GPIO
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
 
+// Definindo os GPIOs
+#define LED_VERMELHO 13
+#define LED_AZUL 12
+#define LED_VERDE 11
+#define BUZZER_A 21
+#define BUZZER_B 10
 
+// Função de inicialização das GPIOs
+void initialize_gpio() {
+    gpio_init(LED_VERMELHO);
+    gpio_set_dir(LED_VERMELHO, GPIO_OUT);
+
+    gpio_init(LED_AZUL);
+    gpio_set_dir(LED_AZUL, GPIO_OUT);
+
+    gpio_init(LED_VERDE);
+    gpio_set_dir(LED_VERDE, GPIO_OUT);
+
+    gpio_init(BUZZER_A);
+    gpio_set_dir(BUZZER_A, GPIO_OUT);
+
+    gpio_init(BUZZER_B);
+    gpio_set_dir(BUZZER_B, GPIO_OUT);
+
+    // Inicializa a UART
+    uart_init(UART_ID, BAUD_RATE);
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+}
 
 int main()
 {
     stdio_init_all();
 
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+    initialize_gpio();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    // Envia uma string de teste na UART
+    uart_puts(UART_ID, "Olá, UART!\n");
+
+    printf("Programa inicializado. Aguardando comandos...\n");
+
+    return 0;
 }
